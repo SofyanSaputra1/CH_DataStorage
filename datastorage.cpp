0b745565b9cc27ac351ba928cee442f936b9c799
Johnil Quezada <johnil@blissfully.com>
2018-06-10 01:24:48 +0800

    Generatorable abi

--- datastorage.cpp
+++ datastorage.cpp
@@ -17,20 +17,18 @@ using eosio::print;
 using eosio::name;
 using std::string;
 
-typedef int delegatedDecisionMaker;
-
 class CHDataStorage : public eosio::contract {
   public:
     CHDataStorage(account_name self) : contract(self),
       profiles(_self, _self),
       medical_providers(_self, _self),
       approved_providers(_self, _self){}
-    int DELEGATED_SELF = 4; // Requires user confirmation
-    int DELEGATED_MP = 5; // Medical Provider decide when researchers get access to data
-    int DELEGATED_CH = 6; // CollectiveHealth decides when researchers get access to data
+    uint8_t DELEGATED_SELF = 4; // Requires user confirmation
+    uint8_t DELEGATED_MP = 5; // Medical Provider decide when researchers get access to data
+    uint8_t DELEGATED_CH = 6; // CollectiveHealth decides when researchers get access to data
 
     //@abi action
-    void createProfile(bool confirmBeforeMPCanStore, bool autoQuery, delegatedDecisionMaker accessor) {
+    void createprofile(bool confirmBeforeMPCanStore, bool autoQuery, uint8_t accessor) {
       account_name owner = _self;
 
       // Temporarily remove this
@@ -50,22 +48,22 @@ class CHDataStorage : public eosio::contract {
     }
 
     //@abi action
-    void createMedicalProvider(account_name mp, string mpId, string firstName, string lastName) {
+    void createmp(account_name mprovider, string mpId, string firstName, string lastName) {
       // Hash the id
       checksum256 mpIdHash;
-      sha256((char *) mpId.c_str(), sizeof(mpId), mpIdHash);
+      sha256((char *) mpId.c_str(), sizeof(mpId), &mpIdHash);
 
       // Store new MP
-      medical_providers.emplace(_self, [&](auto& medical_provider){
-          medical_provider.mp = _self;
-          medical_provider.mpId = mpIdHash;
-          medical_provider.firstName = firstName;
-          medical_provider.lastName = lastName;
-      })
+      medical_providers.emplace(_self, [&](auto& mp){
+          mp.mpName = _self;
+          mp.mpId = mpIdHash;
+          mp.firstName = firstName;
+          mp.lastName = lastName;
+      });
     }
 
     //@abi action
-    void addTrustedMp(account_name mp) {
+    void addtrustedmp(account_name mp) {
       require_auth(mp);
 
       eosio_assert(!hasTrustedMp(mp), "Trusted MP already exists");
@@ -73,8 +71,8 @@ class CHDataStorage : public eosio::contract {
       // Store new trusted MP
       approved_providers.emplace(_self, [&](auto& approved_provider){
           approved_provider.patient = _self;
-          approved_provider.mp = mp;
-      })
+          approved_provider.mprovider = mp;
+      });
     }
 
   private:
@@ -95,11 +93,11 @@ class CHDataStorage : public eosio::contract {
     struct profile {
         profile(
                 account_name owner = account_name(),
-                bool confirmBeforeStore,
-                bool autoQuery,
-                delegatedDecisionMaker accessor
+                bool confirmBeforeMPCanStore = false,
+                bool autoQuery = false,
+                uint8_t accessor = 4
         ):owner(owner),
-          confirmBeforeMPCanStore(confirmBeforeStore),
+          confirmBeforeMPCanStore(confirmBeforeMPCanStore),
           autoQuery(autoQuery),
           accessor(accessor){}
 
@@ -107,19 +105,19 @@ class CHDataStorage : public eosio::contract {
         account_name owner;
         bool confirmBeforeMPCanStore;
         bool autoQuery;
-        delegatedDecisionMaker accessor;
+        uint8_t accessor;
 
         uint64_t primary_key()const { return owner; }
+
+        EOSLIB_SERIALIZE( profile, (owner)(confirmBeforeMPCanStore)(autoQuery)(accessor) )
     };
 
     typedef eosio::multi_index< N(profile), profile> profile_index;
 
     //@abi table medical_providers i64
-    struct medical_provider {
-        medical_provider( account_name mp, checksum256 mpId ):mp(mp), mpId(mpId){}
-
+    struct mprv {
         // The eos owner account_name of the account in this contract
-        account_name mp;
+        account_name mpName;
 
         // Some stringy id to represent the medical provider
         checksum256 mpId;
@@ -128,26 +126,28 @@ class CHDataStorage : public eosio::contract {
 
         string lastName;
 
-        uint64_t primary_key()const { return mp; }
+        uint64_t primary_key()const { return mpName; }
 
+        EOSLIB_SERIALIZE( mprv, (mpName)(mpId)(firstName)(lastName) )
     };
 
-    typedef eosio::multi_index< N(medical_provider), medical_provider> medical_provider_index;
+    typedef eosio::multi_index< N(mprv), mprv> medical_provider_index;
 
     //@abi table approved_providers i64
-    struct approved_provider {
-        approved_provider( account_name patient, account_name mp ): patient(patient), mp(mp){}
-
+    struct apprvMp {
+        account_name mprovider;
         account_name patient;
-        account_name mp;
 
-        uint64_t primary_key()const { return patient; }
-        uint64_t secondary_key()const { return mp; }
+        uint64_t primary_key()const { return mprovider; }
+
+        EOSLIB_SERIALIZE( apprvMp, (mprovider)(patient) )
     };
 
-    typedef eosio::multi_index< N(medical_provider), approved_provider> approved_providers_index;
+    typedef eosio::multi_index< N(apprvMp), apprvMp> approved_providers_index;
 
     profile_index profiles;
     medical_provider_index medical_providers;
     approved_providers_index approved_providers;
 };
+
+EOSIO_ABI(CHDataStorage,(createprofile) (createmp) (addtrustedmp))

ae193539f7a316b7526fb2328fb9d69d1b1ac08b
Johnil Quezada <johnil@blissfully.com>
2018-06-09 13:20:02 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/datastorage.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datastorage.dir/datastorage.cpp.o"
+  "datastorage.pdb"
+  "datastorage"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datastorage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
